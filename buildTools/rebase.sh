ff48b2298d206aaf8cb4014f0bef1b4c87c70dda
dangherve <dangherve@gmail.com>
2026-03-07 01:19:22 -0500

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git remote add upstream https://github.com/jevois/samplemodule.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/samplemodule.git
+git push --force --set-upstream origin master
